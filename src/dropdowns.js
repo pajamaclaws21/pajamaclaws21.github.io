c96f89f4bbb017af689d4bb46b2d19e445d45157
PJ <95423707+pajamaclaws21@users.noreply.github.com>
2024-08-28 14:08:20 -0500

    cleaning up dropdowns.js

--- src/dropdowns.js
+++ src/dropdowns.js
@@ -11,13 +11,4 @@ function makeDropdownsWork() {
   })
 }
 
-/*
-Array.from(document.getElementsByClassName("dropdown")).forEach((item) => {
-  document.getElementById(item.id).addEventListener("click", function() {
-    let dropdown = document.getElementById(`${this.id}Items`);
-    dropdown.hidden = !(dropdown.hidden);
-  })
-});
-*/
-
 document.addEventListener("DOMContentLoaded", makeDropdownsWork);
