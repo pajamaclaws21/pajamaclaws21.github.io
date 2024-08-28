b1076741d0bf8cfd4f00199d762669a82a86e141
PJ <95423707+pajamaclaws21@users.noreply.github.com>
2024-08-28 13:45:39 -0500

    trying to handle multiple dropdown-esque classes

--- src/dropdowns.js
+++ src/dropdowns.js
@@ -1,9 +1,23 @@
 function makeDropdownsWork() {
-    Array.from(document.getElementsByClassName("dropdown")).forEach((item) => {
+  let applicableClasses = ["dropdown", "insideDD"];
+
+  applicableClasses.forEach((classname) => {
+    Array.from(document.getElementsByClassName(classname)).forEach((item) => {
       document.getElementById(item.id).addEventListener("click", function() {
         let dropdown = document.getElementById(`${this.id}Items`);
         dropdown.hidden = !(dropdown.hidden);
       })
     });
-  }
-  document.addEventListener("DOMContentLoaded", makeDropdownsWork);
+  })
+}
+
+/*
+Array.from(document.getElementsByClassName("dropdown")).forEach((item) => {
+  document.getElementById(item.id).addEventListener("click", function() {
+    let dropdown = document.getElementById(`${this.id}Items`);
+    dropdown.hidden = !(dropdown.hidden);
+  })
+});
+*/
+
+document.addEventListener("DOMContentLoaded", makeDropdownsWork);
