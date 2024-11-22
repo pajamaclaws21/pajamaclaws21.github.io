b6b2b7025bbfda224e1a638c12482cd2f353261b
PJ <95423707+pajamaclaws21@users.noreply.github.com>
2024-11-22 13:35:41 -0600

    fixing stuff

--- src/onion/onionring-widget.js
+++ src/onion/onionring-widget.js
@@ -8,6 +8,12 @@
 var tag = document.getElementById(ringID); //find the widget on the page
 
 thisSite = window.location.href; //get the url of the site we're currently on
+
+// Redirecting pj21 netlify & render to be indexed as github
+if (thisSite.includes("pajamaclaws21.netlify.app") || thisSite.includes("pajamaclaws21.onrender.com")){
+  thisSite = "https://pajamaclaws21.github.io";
+}
+
 thisIndex = null;
 
 // go through the site list to see if this site is on it and find its position
