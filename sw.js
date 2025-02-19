4a702880a6df730c9177dc71f57e37699286e56c
PJ <95423707+pajamaclaws21@users.noreply.github.com>
2025-02-19 13:56:45 -0600

    new poems & poetry update.
    two new poems: "Pornography" by Richard Siken and "How Do I Love You?" by Mary Oliver, + updating the service worker file so hopefully poems'll load on my phone, + changing the poetry CSS & the main poetry index so it is easier to read

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var pages = ["/", "/blog", "/poetry/", "/expirements/", "/templates/", "/babytools", "/webring.html", "/offline.html"];
+var pages = ["/", "/blog", "/poetry/", "/poetry/raw/", "/expirements/", "/templates/", "/babytools", "/webring.html", "/offline.html"];
 
 // Service worker code from https://blog.heroku.com/how-to-make-progressive-web-app
 
